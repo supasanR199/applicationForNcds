447bffcd67e201d06885fd2cd1bd03ad19faeb9f
supasan romges <supasanr.1999@gmail.com>
2021-11-23 10:38:18 +0700

    try to add random color but can't work and add desing Postpage Patientpage

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class MainPage extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _MainPage();
@@ -9,96 +8,186 @@ class MainPage extends StatefulWidget {
 class _MainPage extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-  //   return DefaultTabController(
-  //     initialIndex: 1,
-  //     length: 3,
-  //     child: Scaffold(
-  //       appBar: AppBar(
-  //         title: Text(
-  //           "ติดตามผู้ป่วย NCDs\nโรงพยาบาลส่งเสริมสุขภาพตำบล",
-  //           style: TextStyle(color: Colors.black),
-  //         ),
-  //         backgroundColor: Colors.white,
-  //         actions: <Widget>[
-  //           TabBar(tabs: [
-  //             Tab(
-  //               child: Text("ผู้ป่วย"),
-  //             ),
-  //             Tab(
-  //               child: Text("โพสต์"),
-  //             ),
-  //             Tab(
-  //               child: Text("ออกจากระบบ"),
-  //             ),
-  //           ])
-  //           // TextButton(onPressed: null, child: Text("ผู้ป่วย")),
-  //           // TextButton(onPressed: null, child: Text("โพสต์")),
-  //           // TextButton(onPressed: null, child: Text("ออกจากระบบ")),
-  //         ],
-  //         // bottom: TabBar(
-  //         //     tabs: <Widget>[
-  //         //       Tab(
-  //         //         icon: Icon(Icons.cloud_outlined),
-  //         //       ),
-  //         //       Tab(
-  //         //         icon: Icon(Icons.beach_access_sharp),
-  //         //       ),
-  //         //       Tab(
-  //         //         icon: Icon(Icons.brightness_5_sharp),
-  //         //       ),
-  //         //     ],
-  //         //   ),
-  //       ),
-  //       body: Container(
-  //         child: Center(
-  //           child: Card(
-  //             child: SizedBox(
-  //               height: 700,
-  //               width: 1000,
-  //               child: ListView(
-  //                 children: <Widget>[],
-  //               ),
-  //             ),
-  //             //  margin: EdgeInsets.only(top: 100,bottom: 400,),
-  //           ),
-  //         ),
-  //       ),
-  //       backgroundColor: Color.fromRGBO(255, 211, 251, 1),
-  //     ),
-  //   );
-  return DefaultTabController(
+    //   return DefaultTabController(
+    //     initialIndex: 1,
+    //     length: 3,
+    //     child: Scaffold(
+    //       appBar: AppBar(
+    //         title: Text(
+    //           "ติดตามผู้ป่วย NCDs\nโรงพยาบาลส่งเสริมสุขภาพตำบล",
+    //           style: TextStyle(color: Colors.black),
+    //         ),
+    //         backgroundColor: Colors.white,
+    //         actions: <Widget>[
+    //           TabBar(tabs: [
+    //             Tab(
+    //               child: Text("ผู้ป่วย"),
+    //             ),
+    //             Tab(
+    //               child: Text("โพสต์"),
+    //             ),
+    //             Tab(
+    //               child: Text("ออกจากระบบ"),
+    //             ),
+    //           ])
+    //           // TextButton(onPressed: null, child: Text("ผู้ป่วย")),
+    //           // TextButton(onPressed: null, child: Text("โพสต์")),
+    //           // TextButton(onPressed: null, child: Text("ออกจากระบบ")),
+    //         ],
+    //         // bottom: TabBar(
+    //         //     tabs: <Widget>[
+    //         //       Tab(
+    //         //         icon: Icon(Icons.cloud_outlined),
+    //         //       ),
+    //         //       Tab(
+    //         //         icon: Icon(Icons.beach_access_sharp),
+    //         //       ),
+    //         //       Tab(
+    //         //         icon: Icon(Icons.brightness_5_sharp),
+    //         //       ),
+    //         //     ],
+    //         //   ),
+    //       ),
+    //       body: Container(
+    //         child: Center(
+    //           child: Card(
+    //             child: SizedBox(
+    //               height: 700,
+    //               width: 1000,
+    //               child: ListView(
+    //                 children: <Widget>[],
+    //               ),
+    //             ),
+    //             //  margin: EdgeInsets.only(top: 100,bottom: 400,),
+    //           ),
+    //         ),
+    //       ),
+    //       backgroundColor: Color.fromRGBO(255, 211, 251, 1),
+    //     ),
+    //   );
+    return DefaultTabController(
       initialIndex: 1,
       length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "ติดตามผู้ป่วย NCDs\nโรงพยาบาลส่งเสริมสุขภาพตำบล",
-            style: TextStyle(color: Colors.black),
+      child: Container(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "ติดตามผู้ป่วย NCDs\nโรงพยาบาลส่งเสริมสุขภาพตำบล",
+              style: TextStyle(color: Colors.black),
+            ),
+            backgroundColor: Colors.white,
+            bottom: TabBar(
+              indicatorColor: Color.fromRGBO(255, 211, 251, 1),
+              tabs: <Widget>[
+                Tab(
+                  text: 'ผู้ป่วย',
+                  icon: Icon(Icons.cloud_outlined),
+                ),
+                Tab(
+                  text: 'โพสต์',
+                  icon: Icon(Icons.beach_access_sharp),
+                ),
+              ],
+            ),
           ),
-          backgroundColor: Colors.white,
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(
-                text: 'ผู้ป่วย',
-                icon: Icon(Icons.cloud_outlined),
+          body: TabBarView(
+            children: <Widget>[
+              Center(
+                child: buildPatientPage(context),
               ),
-              Tab(
-                text: 'โพสต์',
-                icon: Icon(Icons.beach_access_sharp),
+              Center(
+                child: buildPostPage(context),
               ),
             ],
           ),
         ),
-        body: const TabBarView(
+      ),
+    );
+  }
+
+  Widget buildPostPage(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: 700,
+        width: 1000,
+        child: Column(
           children: <Widget>[
             Center(
-              child: Text("It's cloudy here"),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 10,
+                ),
+                child: Text(
+                  'โพสต์แนะนำ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 40),
+                ),
+              ),
             ),
-            Center(
-              child: Text("It's rainy here"),
-            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  buildContentPost(context),
+                  bulidButtonAddPost(context),
+                ],
+              ),
+            )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildPatientPage(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: 700,
+        width: 1000,
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 10,
+                ),
+                child: Text(
+                  'ผู้ป่วย',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 40),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget bulidButtonAddPost(BuildContext context) {
+    return MaterialButton(
+      onPressed: () {},
+      color: Color.fromRGBO(255, 211, 251, 1),
+      textColor: Colors.white,
+      child: Icon(
+        Icons.add,
+        size: 24,
+      ),
+      padding: EdgeInsets.all(16),
+      shape: CircleBorder(),
+    );
+  }
+
+  Widget buildContentPost(BuildContext context) {
+    return Card(
+      color: Colors.amber,
+      child: SizedBox(
+        height: 200,
+        width: 90,
       ),
     );
   }
