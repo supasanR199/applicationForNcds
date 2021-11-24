fc4a24b165417f6301911139aebc5f3608fbfde7
supasan romges <supasanr.1999@gmail.com>
2021-11-24 22:33:40 +0700

    update ui addpost page.

--- lib/AddPost.dart
+++ lib/AddPost.dart
@@ -25,40 +25,120 @@ class _AddPost extends State<AddPost> {
               child: SizedBox(
                 height: 700,
                 width: 1000,
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          // child: buildButtonLogin(context),
-                          padding: EdgeInsets.only(
-                            left: 50,
-                            right: 20,
-                            top: 70,
-                          ),
-                        ),
-                        Padding(
-                          // child: buildButtonRegister(context),
-                          padding: EdgeInsets.only(
-                            left: 0,
-                            right: 50,
-                            top: 70,
-                          ),
-                        ),
-                        // buildButtonLogin(context),
-                        // buildButtonRegister(context),
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ),
-                  ],
-                ),
+                child: buildInputPostFrom(context),
               ),
-              //  margin: EdgeInsets.only(top: 100,bottom: 400,),
             ),
           ),
         ),
-          backgroundColor: Color.fromRGBO(255, 211, 251, 1),
+        backgroundColor: Color.fromRGBO(255, 211, 251, 1),
       ),
+    );
+  }
+
+  Widget buildInputPostFrom(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'หัวเรื่อง',
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'ไฮไลต์',
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            bottom: 5,
+            top: 20,
+            right: 20,
+            left: 20,
+          ),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'เนื้อหาโพสต์',
+              textAlign: TextAlign.left,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            bottom: 20,
+            top: 0,
+            right: 20,
+            left: 20,
+          ),
+          child: TextField(
+            keyboardType: TextInputType.multiline,
+            textInputAction: TextInputAction.newline,
+            maxLines: 10,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'เนื้อหาโพสต์',
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: RaisedButton(
+            // color: Colors.accents,
+            onPressed: () => Navigator.pushNamed(context, '/mainpage'),
+            color: Colors.white,
+            child: Text('ฮัพโหลดรูปภาพ'),
+            padding: EdgeInsets.all(20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(4),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: RaisedButton(
+                onPressed: () => Navigator.pushNamed(context, '/mainpage'),
+                color: Colors.green[100],
+                child: Text('บันทึก'),
+                padding: EdgeInsets.all(20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(4),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: RaisedButton(
+                onPressed: () => Navigator.pushNamed(context, '/mainpage'),
+                color: Colors.red[100],
+                child: Text('ยกเลิก'),
+                padding: EdgeInsets.all(20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(4),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
